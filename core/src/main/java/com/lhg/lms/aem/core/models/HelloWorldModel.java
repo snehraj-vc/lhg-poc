58289cc554999549c2bebf7dabd4dc4e7da8c118
Pradeep <pradeep.dubey@verticurl.com>
2023-01-19 13:10:26 +0530

    fixed HelloWorld loading issue

--- core/src/main/java/com/lhg/lms/aem/core/models/HelloWorldModel.java
+++ core/src/main/java/com/lhg/lms/aem/core/models/HelloWorldModel.java
@@ -15,26 +15,14 @@
  */
 package com.lhg.lms.aem.core.models;
 
-import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
-
-import java.util.Optional;
-
-import javax.annotation.PostConstruct;
-
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
-import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.export.json.ExporterConstants;
-import com.day.cq.wcm.api.Page;
-import com.day.cq.wcm.api.PageManager;
 
 @Model(adaptables = SlingHttpServletRequest.class,
 adapters = {ComponentExporter.class},
@@ -42,33 +30,10 @@ resourceType = HelloWorldModel.RESOURCE_TYPE)
 @Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
 public class HelloWorldModel implements ComponentExporter {
 
-    @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
-    @Default(values="No resourceType")
-    protected String resourceType;
-    
     protected static final String RESOURCE_TYPE = "lhg-lms/components/helloworld";
-
-    @SlingObject
-    private Resource currentResource;
-    @SlingObject
-    private ResourceResolver resourceResolver;
     
-    @ValueMapValue
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
     protected String text;
-    
-    private String message;
-
-    @PostConstruct
-    protected void init() {
-        PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
-        String currentPagePath = Optional.ofNullable(pageManager)
-                .map(pm -> pm.getContainingPage(currentResource))
-                .map(Page::getPath).orElse("");
-
-        message = "Hello World!\n"
-            + "Resource type is: " + resourceType + "\n"
-            + "Current page is:  " + currentPagePath + "\n";
-    }
 
     public String getText() {
         return text;
