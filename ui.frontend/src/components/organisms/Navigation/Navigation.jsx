9e2c7b63bda2ad494d75359a8cae0371e8ca2be1
Sneh Raj Lakhotia <snehraj.lakhotia@verticurl.com>
2023-01-21 16:33:31 +0530

    sanitizing files

--- ui.frontend/src/components/organisms/Navigation/Navigation.jsx
+++ ui.frontend/src/components/organisms/Navigation/Navigation.jsx
@@ -6,17 +6,17 @@ const Navigation = (props) => {
         items = []
     } = props;
     return (<div id={id}>
-         {items.length ?
-                        items.map((item, key) => {
-                            const {
-                                id,
-                                title,
-                                url
-                            } = item;
-                            return (<a href={url} key={key} id={id}>{title}</a>)
-                        })
-                : null
-            }
+        {items.length ?
+            items.map((item, key) => {
+                const {
+                    id,
+                    title,
+                    url
+                } = item;
+                return (<a href={url} key={key} id={id}>{title}</a>)
+            })
+            : null
+        }
     </div>)
 };
 
