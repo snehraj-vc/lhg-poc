6f32b57deacb082ca3d1480b8f3d7a0f505bc065
Sneh Raj Lakhotia <snehraj.lakhotia@verticurl.com>
2023-01-24 20:31:08 +0530

    story book setup

---
+++ ui.frontend/src/components/atoms/Text/Text.stories.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Text from './Text';
+
+export default {
+    title: 'Atoms/Text',
+    component: Text,
+    argsTypes: {}
+}
+
+const Template = (args) => <Text {...args} />;
+
+export const PlainText = Template.bind({});
+
+PlainText.args = {
+    text: 'Hello',
+    richText: false
+}
+
+export const RichText = Template.bind({});
+
+RichText.args = {
+    text: '<p>Hello<p>',
+    richText: true
+}
