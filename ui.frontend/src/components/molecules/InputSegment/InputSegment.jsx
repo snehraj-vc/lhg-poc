eb54927cefcc5f8a64bcec3189ed423d615828ff
Sneh Raj Lakhotia <snehraj.lakhotia@verticurl.com>
2023-01-25 11:38:51 +0530

    input and label components

---
+++ ui.frontend/src/components/molecules/InputSegment/InputSegment.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+    Input,
+    Label
+} from '../../atoms';
+
+const InputSegment = (props) => {
+    const {
+        inputWithLabel=true,
+        className="",
+        id="",
+        name="",
+        required=false,
+        placeholder="",
+        labelText="",
+        inputType="text"
+    } = props;
+
+    return (
+        <div className="input-segment">
+            {inputWithLabel && <Label id={id} className={className} text={labelText} />}
+            <Input id={id} name={name} type={inputType} required={required} placeholder={placeholder} className={className} />
+        </div>
+    )
+};
+
+export default InputSegment;
