ca29f27ceccee515827d9762292af42f10cd5be5
Sneh Raj Lakhotia <snehraj.lakhotia@verticurl.com>
2023-02-06 13:43:22 +0530

    olapic changes

--- ui.frontend/src/components/organisms/Olapic/Olapic.jsx
+++ ui.frontend/src/components/organisms/Olapic/Olapic.jsx
@@ -3,31 +3,19 @@ import React, {useEffect, useRef} from 'react';
 const Olapic = (props) => {
     const {
         id="",
-        scriptLink="",
-        widgetName="",
-        instance="",
-        apikey="",
-        async="async"
+        html=""
     } = props;
 
     const olapicRef = useRef(null);
 
     useEffect(() => {
-        if(!document.querySelector(`script[src="${scriptLink}"]`)) {
-            const s = document.createElement('script');
-            s.type = 'text/javascript';
-            s.async = async;
-            s.src=scriptLink;
-            s.dataset.olapic = widgetName;
-            s.dataset.instance = instance;
-            s.dataset.apikey = apikey;
-            document.body.appendChild(s);
-        }
+        const fragment = document.createRange().createContextualFragment(html);
+        olapicRef.current.append(fragment);
     });
 
     return (
         <>
-            <div id={id} ref={olapicRef}></div>
+            {html && (<div id={id} ref={olapicRef}></div>)}
         </>
     )
 };
--- ui.frontend/src/components/organisms/Olapic/Olapic.stories.jsx
+++ ui.frontend/src/components/organisms/Olapic/Olapic.stories.jsx
@@ -13,9 +13,5 @@ export const OlapicComp = Template.bind({});
 
 OlapicComp.args = {
     id: "olapic_specific_widget",
-    scriptLink: "https://photorankstatics-a.akamaihd.net/743d2e78a76dedeb07e0745158547931/static/frontend/latest/build.min.js",
-    widgetName: "olapic_specific_widget",
-    instance: "4017de735bd205099a2ece082829be73",
-    apikey: "cfba7d3b691826a3758e420e817cffa5d5cfd447f40aa7ec16678610bdf4ebbf",
-    async: "async"
+    html: '<div id="olapic_specific_widget"></div><script type="text/javascript" src="https://photorankstatics-a.akamaihd.net/743d2e78a76dedeb07e0745158547931/static/frontend/latest/build.min.js" data-olapic="olapic_specific_widget" data-instance="4017de735bd205099a2ece082829be73" data-apikey="cfba7d3b691826a3758e420e817cffa5d5cfd447f40aa7ec16678610bdf4ebbf" async="async"></script>',
 }
