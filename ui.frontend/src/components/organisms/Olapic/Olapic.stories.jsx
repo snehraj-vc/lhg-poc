2fc6762141b958d9421618985fabc3fd8810f29e
Sneh Raj Lakhotia <snehraj.lakhotia@verticurl.com>
2023-02-06 14:33:02 +0530

    ola pic story update

--- ui.frontend/src/components/organisms/Olapic/Olapic.stories.jsx
+++ ui.frontend/src/components/organisms/Olapic/Olapic.stories.jsx
@@ -12,6 +12,5 @@ const Template = (args) => <Olapic {...args} />;
 export const OlapicComp = Template.bind({});
 
 OlapicComp.args = {
-    id: "olapic_specific_widget",
     html: '<div id="olapic_specific_widget"></div><script type="text/javascript" src="https://photorankstatics-a.akamaihd.net/743d2e78a76dedeb07e0745158547931/static/frontend/latest/build.min.js" data-olapic="olapic_specific_widget" data-instance="4017de735bd205099a2ece082829be73" data-apikey="cfba7d3b691826a3758e420e817cffa5d5cfd447f40aa7ec16678610bdf4ebbf" async="async"></script>',
 }
