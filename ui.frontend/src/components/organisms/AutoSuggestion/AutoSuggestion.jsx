4321170e2a8bd365c6eac31ec66db4b955bfae11
sabarinathan_vc <sabarinathan.nagarajan@verticurl.com>
2023-02-20 11:16:28 +0530

    Auto suggestion update

--- ui.frontend/src/components/organisms/AutoSuggestion/AutoSuggestion.jsx
+++ ui.frontend/src/components/organisms/AutoSuggestion/AutoSuggestion.jsx
@@ -1,16 +1,36 @@
 import React from "react";
-import { Input,Button } from "../../atoms";
+import { Input } from "../../atoms";
+import { useState } from "react";
+import { SelectOption } from "../../molecules";
+
 const AutoSuggestion = (props) => {
-
-    const {
-       input={},
-       button={},
+    const{
+       hotels=[],
+       name=""
     }=props;
+    
+    const [query,setquery]=useState("");
+    const [mapvalue,setmapvalue]=useState("")
 
+    const onInputChange = (value) => {
+        setquery(value);
+      };
     return ( <>
     
-    <Input placeholder={props.inputs.placeholder} />
-    <Button text={props.button.text}/>
+    <Input placeholder={props.inputPlaceholder} 
+           value={query}
+           onChange={onInputChange}
+     />
+
+    { hotels.map((res)=>{
+          console.log(res.name)
+          
+          return (<div> <h2>{res.continent}</h2><h4>{res.country}</h4><h6>{res.name}</h6></div>)
+        })}
+     <SelectOption  />
+    <div>
+        <h3>{props.recentSearchText}</h3>
+    </div>
 
     </> );
 }
