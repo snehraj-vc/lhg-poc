8c25f6a84ebbb5576e7ce27b7bdf992605f78150
Elamurasu <elamurasu.mullaiyarasu@verticurl.com>
2023-03-09 19:34:32 +0530

    adding style

--- ui.frontend/src/components/organisms/BrilliantFooter/BrilliantFooter.jsx
+++ ui.frontend/src/components/organisms/BrilliantFooter/BrilliantFooter.jsx
@@ -4,20 +4,55 @@ import "./BrilliantFooter.scss";
 const BrilliantFooter = (props) => {
     const {
         headerimage = "",
-        arrayimage = []
+        arrayimage = [],
+        data = [],
+        social_url = []
     } = props
 
-    return (<div className="footer-bg">
-        <div className="footerimg">
-            <img src={headerimage} alt="" />
+    return (<>
+        <div className="footer-bg">
+            <div className="footerimg">
+                <img src={headerimage} alt="" />
+            </div>
+            <div className="arrayimg">
+                {arrayimage.map((res) => {
+                    return (<img src={res.url}></img>)
+                })
+                }
+            </div>
         </div>
-        <div className="arrayimg">
-            {arrayimage.map((res) => {
-                return (<img src={res.url}></img>)
+
+        {
+            <div className="address">
+                <h1>BRILLIANT BY LANGHAM</h1>
+                <p>2701, Great Eagle Centre,</p>
+                <p>23 Harbour road, wan chai, Hong kong</p>
+                <p>info@brilliantbylangham</p>
+            </div>
+        }
+
+        {
+            <div className="social-icons">
+                <div classsName="social-icons-label"><h1>Find us</h1></div>
+
+                {social_url.map((res) => {
+                    return (<img src={res.url}></img>)
+                }
+                )}
+            </div>
+        }
+<ul className="footer-list">
+        {
+            data.map((idx) => {
+                return ( <li>
+                        {idx.title}
+                    </li>
+               )
+
             })
-            }
-        </div>
-    </div>
+        }
+        </ul>
+        </>
     );
 }
 
--- ui.frontend/src/components/organisms/BrilliantFooter/BrilliantFooter.stories.jsx
+++ ui.frontend/src/components/organisms/BrilliantFooter/BrilliantFooter.stories.jsx
@@ -11,17 +11,46 @@ export default {
 const Template = (args) => <BrilliantFooter {...args} />
 
 export const BrilliantFooterData = Template.bind({});
-BrilliantFooterData.args={
-    headerimage :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkTlouzcZmqeZPb1fi3x_cWc3NfJj4sYOHO0J2yvSWO1YCZBT_Pb0N7u1GElzG1ErdQw&usqp=CAU",
-    arrayimage:[
-        {url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU"},
-        {url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU"},
-        {url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU"},
-        {url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU"},
-        {url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU"},
-        {url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU"},
-        {url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU"},
-        {url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU"},
+BrilliantFooterData.args = {
+    headerimage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkTlouzcZmqeZPb1fi3x_cWc3NfJj4sYOHO0J2yvSWO1YCZBT_Pb0N7u1GElzG1ErdQw&usqp=CAU",
+    arrayimage: [
+        { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU" },
+        { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU" },
+        { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU" },
+        { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU" },
+        { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU" },
+        { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU" },
+        { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU" },
+        { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU" }
+    ],
+    social_url: [
+        {
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU"
+        },
+        {
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU"
+        },
+        {
+            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU"
+        },
+        {
+            url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPARP0lJ64JfgJSR62AxBui_p5aAubxXn7aEOnLFWHvrdHhZL4WeyibvTQHBtCUaHNo8&usqp=CAU"
+        }
+    ],
+    data: [
+        {
+            title: "LANGHAM HOSPITALITY GROUPS"
+        }, {
+            title: "BEST RATE GUARANTEE"
+        },
+        {
+            title: "MEDIA CENTRE"
+        },
+        {
+            title: "FAQ"
+        },
+        {
+            title: "CONTACT US"
+        }
     ]
-    
 }
