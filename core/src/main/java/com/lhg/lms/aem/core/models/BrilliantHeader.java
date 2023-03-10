374469feb9c8946fa3cfb5d408c8cc725566de95
Prateek-3909 <prateekkumar.sanjeevkumar@verticurl.com>
2023-03-10 09:56:56 +0530

    brilliantheader multifield updated

--- core/src/main/java/com/lhg/lms/aem/core/models/BrilliantHeader.java
+++ core/src/main/java/com/lhg/lms/aem/core/models/BrilliantHeader.java
@@ -48,16 +48,18 @@ import java.util.ArrayList;
 public class BrilliantHeader implements ComponentExporter {
     private static final Logger logger = LoggerFactory.getLogger(BrilliantHeader.class);
     protected static final String RESOURCE_TYPE = "lhg-lms/components/brilliantheader";
-    private static final String LANG_EN = "EN";
-    private static final String LANG_ZH_HK = "ZH_HK";
-    private static final String LANG_ZH_CN = "ZH_CN";
+//    private static final String LANG_EN = "EN";
+//    private static final String LANG_ZH_HK = "ZH_HK";
+//    private static final String LANG_ZH_CN = "ZH_CN";
 
     @Inject
     private ResourceResolver resourceResolver;
 
-    private String enURI;
-    private String zh_hkURI;
-    private String zh_cnURI;
+//    private String currentPageURI;
+//    private String otherPageURI;
+    private String languageUrls;
+//    private String zh_hkURI;
+//    private String zh_cnURI;
 
     @ScriptVariable
     private Page currentPage;
@@ -65,33 +67,29 @@ public class BrilliantHeader implements ComponentExporter {
     private SlingHttpServletRequest request;
 
 
-    private String currentPageUri;
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String enTitle;
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String zhhkTitle;
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String zhcnTitle;
-
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String enLabel;
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String zhhkLabel;
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String zhcnLabel;
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String enPath;
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String zhhkPath;
-    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
-    private String zhcnPath;
-    private String enFallbackurl="/content/lhg-lms/en/home";
-    private String zhFallbackurl="/content/lhg-lms/zh_hk/home";
-    private String zh_cnFallbackurl="/content/lhg-lms/zh_cn/home";
-
-
-
-    private String redirectURI;
+//    private String currentPageUri;
+//    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+//    private String enTitle;
+//    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+//    private String zhhkTitle;
+//    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+//    private String zhcnTitle;
+//
+//    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+//    private String enLabel;
+//    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+//    private String zhhkLabel;
+//    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+//    private String zhcnLabel;
+//    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+//    private String enPath;
+//    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+//    private String zhhkPath;
+//    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+//    private String zhcnPath;
+//    private String enFallbackurl="/content/lhg-lms/en/home";
+//    private String zhFallbackurl="/content/lhg-lms/zh_hk/home";
+//    private String zh_cnFallbackurl="/content/lhg-lms/zh_cn/home";
 
     @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
     protected String brilliantimage;
@@ -142,6 +140,10 @@ public class BrilliantHeader implements ComponentExporter {
 
     private Resource menuitems;
 
+    @ChildResource(injectionStrategy = InjectionStrategy.OPTIONAL, name = "languagetoggle")
+
+    private Resource languageToggleItems;
+
     private ArrayList<MenuItems> menuItems=new ArrayList<>();
     @PostConstruct
     protected void init() {
@@ -154,114 +156,56 @@ public class BrilliantHeader implements ComponentExporter {
                 items.setItemLink(valueMap.get("itemlink",String.class));
                 menuItems.add(items);
             }}
+        logger.info("toggle1");
 
-        if (StringUtils.isNotEmpty(enLabel) && enLabel.equalsIgnoreCase(LANG_EN)) {
-            if (zhFallbackurl == null || zhFallbackurl.isEmpty() || zhhkPath == null || zhhkPath.isEmpty()) {
-                redirectURI = null;
-            }
-
-            currentPageUri=currentPage.getPath().concat(".html");
+        if(languageToggleItems != null){
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonArray array = new JsonArray();
+            //currentPageUri = currentPage.getPath().concat(".html");
             String currentPagePathUrl = currentPage.getPath();
-            String urlLocal= String.valueOf(currentPage.getLanguage());
-            if(urlLocal.equalsIgnoreCase(LANG_EN)){
-                enURI=currentPagePathUrl.concat(".html");
-                zh_hkURI=languageurlgenerator(currentPagePathUrl,enPath,zhhkPath).concat(".html");
-                zh_cnURI=languageurlgenerator(currentPagePathUrl,enPath,zhcnPath).concat(".html");
+            String currentPageLanguage = String.valueOf(currentPage.getLanguage().toString().toLowerCase());
+            logger.info("currentPagePathUrl : {}",currentPagePathUrl);
+            logger.info("currentPageLanguage : {}",currentPageLanguage);
 
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                JsonArray array = new JsonArray();
-                HeaderLanguage en = new HeaderLanguage();
-                en.setTitle(enTitle);
-                en.setLink(enURI);
-                HeaderLanguage zh_hk = new HeaderLanguage();
-                zh_hk.setTitle(zhhkTitle);
-                zh_hk.setLink(zh_hkURI);
-                HeaderLanguage zh_cn = new HeaderLanguage();
-                zh_cn.setTitle(zhcnTitle);
-                zh_cn.setLink(zh_cnURI);
-                array.add(gson.toJson(en, HeaderLanguage.class));
-                array.add(gson.toJson(zh_hk, HeaderLanguage.class));
-                array.add(gson.toJson(zh_cn, HeaderLanguage.class));
-                languages = gson.toJson(array, JsonArray.class);
-                logger.info("languages :::: {}", languages);
+            Iterable<Resource> multiLanguage = languageToggleItems.getChildren();
+            for (Resource multiLanguageResource : multiLanguage){
+                ValueMap valueMap = multiLanguageResource.getValueMap();
+                LanguageToggle languageToggle=new LanguageToggle();
+                languageToggle.setLanguageLabel(valueMap.get("languageLabel",String.class));
+                languageToggle.setLanguagePath(valueMap.get("languagePath",String.class));
+                languageToggle.setLanguageTitle(valueMap.get("languageTitle",String.class));
+                languageToggle.setLanguageConstant(valueMap.get("languageConstant",String.class));
+
+                    if (StringUtils.isNotEmpty(languageToggle.getLanguageLabel())
+                            && languageToggle.getLanguageLabel().equalsIgnoreCase(languageToggle.getLanguageConstant())) {
+                        //String multiListResourceLanguage = String.valueOf(currentPage.getLanguage());
+                        /*currentPageURI = currentPagePathUrl.concat(".html");*/
+                        logger.info("languageToggle.getLanguageLabel() : {}",languageToggle.getLanguageLabel());
+                        languageUrls = currentPagePathUrl.replace("/"+currentPageLanguage, "/"+languageToggle.getLanguageLabel()).concat(".html");
+                        //otherPageURI = languageurlgenerator(currentPagePathUrl,currentPageURI, languageToggle.getLanguagePath()).concat(".html");
+                        logger.info("otherPageURI : {}", languageUrls);
+
+                        HeaderLanguage language = new HeaderLanguage();
+                        language.setTitle(languageToggle.getLanguageTitle());
+                        language.setLink(languageUrls);
+//
+                        array.add(gson.toJson(language, HeaderLanguage.class));
+                    }
             }
-
-        }
-
-        if (StringUtils.isNotEmpty(zhhkLabel) && zhhkLabel.equalsIgnoreCase(LANG_ZH_HK)) {
-            if (enFallbackurl == null || enFallbackurl.isEmpty() || enPath == null || enPath.isEmpty()) {
-                redirectURI = null;
-            }
-
-
-            currentPageUri=currentPage.getPath().concat(".html");
-            String currentPagePathUrl = currentPage.getPath();
-            String urlLocal= String.valueOf(currentPage.getLanguage());
-            if(urlLocal.equalsIgnoreCase(LANG_ZH_HK)){
-                zh_hkURI=currentPagePathUrl.concat(".html");
-                enURI=languageurlgenerator(currentPagePathUrl,zhhkPath,enPath).concat(".html");
-                zh_cnURI=languageurlgenerator(currentPagePathUrl,zhhkPath,zhcnPath).concat(".html");
-
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                JsonArray array = new JsonArray();
-                HeaderLanguage en = new HeaderLanguage();
-                en.setTitle(enTitle);
-                en.setLink(enURI);
-                HeaderLanguage zh_hk = new HeaderLanguage();
-                zh_hk.setTitle(zhhkTitle);
-                zh_hk.setLink(zh_hkURI);
-                HeaderLanguage zh_cn = new HeaderLanguage();
-                zh_cn.setTitle(zhcnTitle);
-                zh_cn.setLink(zh_cnURI);
-                array.add(gson.toJson(en, HeaderLanguage.class));
-                array.add(gson.toJson(zh_hk, HeaderLanguage.class));
-                array.add(gson.toJson(zh_cn, HeaderLanguage.class));
-                languages = gson.toJson(array, JsonArray.class);
-                logger.info("languages :::: {}", languages);
-
-            }
-        }
-
-        if (StringUtils.isNotEmpty(zhcnLabel) && zhcnLabel.equalsIgnoreCase(LANG_ZH_CN)) {
-            if (enFallbackurl == null || enFallbackurl.isEmpty() || enPath == null || enPath.isEmpty()) {
-                redirectURI = null;
-            }
-
-
-            currentPageUri=currentPage.getPath().concat(".html");
-            String currentPagePathUrl = currentPage.getPath();
-            String urlLocal= String.valueOf(currentPage.getLanguage());
-            if(urlLocal.equalsIgnoreCase(LANG_ZH_CN)){
-                zh_cnURI=currentPagePathUrl.concat(".html");
-                enURI=languageurlgenerator(currentPagePathUrl,zhcnPath,enPath).concat(".html");
-                zh_hkURI=languageurlgenerator(currentPagePathUrl,zhcnPath,zhhkPath).concat(".html");
-
-                Gson gson = new GsonBuilder().setPrettyPrinting().create();
-                JsonArray array = new JsonArray();
-                HeaderLanguage en = new HeaderLanguage();
-                en.setTitle(enTitle);
-                en.setLink(enURI);
-                HeaderLanguage zh_hk = new HeaderLanguage();
-                zh_hk.setTitle(zhhkTitle);
-                zh_hk.setLink(zh_hkURI);
-                HeaderLanguage zh_cn = new HeaderLanguage();
-                zh_cn.setTitle(zhcnTitle);
-                zh_cn.setLink(zh_cnURI);
-                array.add(gson.toJson(en, HeaderLanguage.class));
-                array.add(gson.toJson(zh_hk, HeaderLanguage.class));
-                array.add(gson.toJson(zh_cn, HeaderLanguage.class));
-                languages = gson.toJson(array, JsonArray.class);
-                logger.info("languages :::: {}", languages);
-            }
+            languages = gson.toJson(array, JsonArray.class);
         }
 
     }
-    private String languageurlgenerator(String currentPage ,String sourcePath, String destinationPath){
-        if (sourcePath != null) {
-            redirectURI = currentPage.replace(sourcePath, destinationPath);
-        }
-        return redirectURI;
-    }
+//    private String languageurlgenerator(String currentPage ,String sourcePath, String destinationPath){
+//        logger.info("currentPage :::: {}", currentPage);
+//        logger.info("sourcePath :::: {}", sourcePath);
+//        logger.info("destinationPath :::: {}", destinationPath);
+//        String redirectURI=null;
+//        if (sourcePath != null) {
+//            redirectURI = currentPage.replace(sourcePath, destinationPath);
+//        }
+//        return redirectURI;
+//    }
 
     public ArrayList<MenuItems> getMenuItems() {
         return menuItems;
@@ -319,7 +263,6 @@ class MenuItems {
 class HeaderLanguage {
     private String title;
     private String link;
-    public HeaderLanguage(){}
 
     public String getTitle() {
         return title;
@@ -335,5 +278,44 @@ class HeaderLanguage {
 
     public void setLink(String link) {
         this.link = link;
+    }
+}
+
+class LanguageToggle{
+    private String languageLabel;
+    private String languagePath;
+    private String languageTitle;
+    private String languageConstant;
+
+    public void setLanguageLabel(String languageLabel) {
+        this.languageLabel = languageLabel;
+    }
+
+    public String getLanguageLabel() {
+        return languageLabel;
+    }
+
+    public void setLanguagePath(String languagePath) {
+        this.languagePath = languagePath;
+    }
+
+    public String getLanguagePath() {
+        return languagePath;
+    }
+
+    public void setLanguageTitle(String languageTitle) {
+        this.languageTitle = languageTitle;
+    }
+
+    public String getLanguageTitle() {
+        return languageTitle;
+    }
+
+    public void setLanguageConstant(String languageConstant) {
+        this.languageConstant = languageConstant;
+    }
+
+    public String getLanguageConstant() {
+        return languageConstant;
     }
 }
