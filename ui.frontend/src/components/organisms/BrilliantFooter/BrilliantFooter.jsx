99046e51944264821a6309f848487ebe5e76bbbf
sabarinathan_vc <sabarinathan.nagarajan@verticurl.com>
2023-03-14 01:08:08 +0530

    updated the footer part

--- ui.frontend/src/components/organisms/BrilliantFooter/BrilliantFooter.jsx
+++ ui.frontend/src/components/organisms/BrilliantFooter/BrilliantFooter.jsx
@@ -1,59 +1,65 @@
 import React from "react";
-import "./BrilliantFooter.scss";
-
+import './BrilliantFooter.scss'
 const BrilliantFooter = (props) => {
-    const {
-        headerimage = "",
-        arrayimage = [],
-        data = [],
-        social_url = []
-    } = props
 
+    const {
+        brandsList = [],
+        brandLogoPath = "",
+        brandLogoAltText = "",
+        contactInfo = "",
+        findUs = "",
+        socialIconsList = "",
+        secondLevelList = [],
+        thirdLevelList = [],
+        copyrightText = ""
+
+    } = props;
     return (<>
-        <div className="footer-bg">
-            <div className="footerimg">
-                <img src={headerimage} alt="" />
-            </div>
-            <div className="arrayimg">
-                {arrayimage.map((res) => {
-                    return (<img src={res.url}></img>)
+        <div className="footer-container">
+            <div className="footer-HeaderBrandLog"><img src={brandLogoPath} alt={brandLogoAltText} /></div>
+            <div className="footer-brandlogs">
+                {brandsList.map((res) => {
+                    return (<img src={res.logoPath} alt={res.altText} />);
                 })
                 }
             </div>
         </div>
+        <div className="footer-contactContainer">
+            <div className="footer-contactHeader">BRILLIANT BY LANGHAM</div>
+            <div className="footer-contactAddress">
+                <p>2701, Great Eagle Centre, 23 Harbour Road, Wan Chai, Hong Kong info@brilliantbylangham
+                    F: +852 2186 2300 info@langhamhotels.com
+                </p>
 
-        {
-            <div className="address">
-                <h1>BRILLIANT BY LANGHAM</h1>
-                <p>2701, Great Eagle Centre,</p>
-                <p>23 Harbour road, wan chai, Hong kong</p>
-                <p>info@brilliantbylangham</p>
             </div>
-        }
+            <div className="footerHorizontalLine-1"></div>
+            <div className="footer-socialicons">
+                <div className="iconLabel">{findUs}</div>
+                <div className="socialIcons"> {socialIconsList.map((res) => {
+                    return (<img src={res.logoPath} alt={res.altText} />)
+                })}</div>
 
-        {
-            <div className="social-icons">
-                <div classsName="social-icons-label"><h1>Find us</h1></div>
+            </div>
+            <div className="footerHorizontalLine-2"></div>
+            <div className="footer-secondLevelList">
+                {secondLevelList.map((res) => {
+                    return (<a href={res.itemPath}>{res.itemText}</a>)
+                })
 
-                {social_url.map((res) => {
-                    return (<img src={res.url}></img>)
                 }
-                )}
             </div>
-        }
-<ul className="footer-list">
-        {
-            data.map((idx) => {
-                return ( <li>
-                        {idx.title}
-                    </li>
-               )
+            <div className="footerHorizontalLine-3"></div>
+            <div className="footer-thirdLevelList">
 
-            })
-        }
-        </ul>
-        </>
-    );
+                {thirdLevelList.map((res) => {
+                    return (<a href={res.itemPath}>{res.itemText} |</a>)
+                })
+                }
+
+                <p>{copyrightText}</p>
+            </div>
+        </div>
+    </>);
 }
 
 export default BrilliantFooter;
